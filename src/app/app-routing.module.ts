4dce1bded70f33fb7efe3ee920e280860dd71b20
Sebastian <jsuseche1@hotmail.com>
2019-05-14 10:57:44 -0500

    se añadio carpeta perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  },  { path: 'restaurantes', loadChildren: './restaurantes/restaurantes.module#RestaurantesPageModule' }
+  },  { path: 'restaurantes', loadChildren: './restaurantes/restaurantes.module#RestaurantesPageModule' },
+  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' }
 
 ];
 
